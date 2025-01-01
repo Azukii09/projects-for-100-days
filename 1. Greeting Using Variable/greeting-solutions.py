27fb24f0b7c245c061e75859c7cc216a1340a78e
alvillage <azuknisianta@gmail.com>
2025-01-01 10:54:34 +0700

    ## - solutions for day 1 problem, make greeting script using variable in python
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 1. Greeting Using Variable/greeting-solutions.py
+++ 1. Greeting Using Variable/greeting-solutions.py
@@ -1,3 +1,9 @@
+# make new variable call name, and assign using input method.
+# input() is built in function from python that allow user to input from keyboard
 name = input("What is your name? ")
+
+# Prints to the standard output device. The code below prints the string combined with the string from the variable filled in via the input above
 print("Hello, " + name + ".")
+
+# Print sting to the standard output device.
 print("Wellcome to the Python Programming Journey ")
