7b7aa122842549c812907d54aa9bc0e3a6c4d740
alvillage <azuknisianta@gmail.com>
2025-01-02 08:40:16 +0700

    ## - day 2 solutions, make "Band Name Generator" script using combining 2 variable in python
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 2. Band name generator/band-name-solutions.py
+++ 2. Band name generator/band-name-solutions.py
@@ -1,4 +1,11 @@
+# 1. Create a greeting for your program.
 print("Welcome to the Group Name Generator.")
+
+# 2. Ask the user for their favorite color.
 color = input("What's your favorite color? ")
+
+# 3. Ask the user for their favorite animal.
 animal = input("What's your favorite animal? ")
+
+# 4. Combine the name of their favorite color and animal and show them their group name.
 print(f"Your group name could be {color} {animal}")
