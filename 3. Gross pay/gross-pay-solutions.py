953bc53982b6302c029a8952a239df4a2663e967
alvillage <azuknisianta@gmail.com>
2025-01-03 08:54:16 +0700

    ## - day 3 solutions, make "Gross Pay" script using combining 2 variable in python to compute gross pay base on rate and hours
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 3. Gross pay/gross-pay-solutions.py
+++ 3. Gross pay/gross-pay-solutions.py
@@ -1,5 +1,12 @@
+# make variable input
 hours = float(input("Enter Hours: "))
 rate = float(input("Enter Rate ($): "))
 
+# make calculations to calculate gross pay by multiplying the rate by the hours worked
+# after that round the results using the built-in function round() with a exactly two digits after the decimal place.
 paid = round(hours * rate,2)
+
+# Print output to the standard output device. This is using string formating.
 print(f"You must be paid the following amount: $ {paid}")
+
+# note: you can check python built-in function in https://docs.python.org/3/library/functions.html
