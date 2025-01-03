719943ba420bddab26482f0fdc175397cda42e8a
alvillage <azuknisianta@gmail.com>
2025-01-04 05:49:05 +0700

    ## - solutions day 4 problem, make "Celsius to Fahrenheit Converter" script using Formula (Celsius x 9/5)+Fahrenheit in python to convert temperature
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 4. Celsius to Fahrenheit /celsius-to-fahrenheit-solutions.py
+++ 4. Celsius to Fahrenheit /celsius-to-fahrenheit-solutions.py
@@ -1,4 +1,11 @@
+# make variable input and convert string input to int format
 celsius = int(input("Enter Temperature in Celsius: "))
+
+# make calculations to convert Celsius to Fahrenheit
+# after that round the results using the built-in function round().
 fahrenheit = round((celsius * 9 / 5) + 32)
+
+# Print output to the standard output device. This is using string formating.
 print(f"{celsius} Celsius = {fahrenheit} Fahrenheit")
 
+# note: you can check python built-in function in https://docs.python.org/3/library/functions.html
