aef766a2b445984706ac9cb35774c69d0ea355d0
alvillage <azuknisianta@gmail.com>
2025-01-06 06:03:57 +0700

    ## - Solutions day 6 problem, make "Gross Pay With Overtime" script using combining 2 variable in python to compute gross pay base on rate and hours, but in this case we will consider the overtime variable
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 6. Gross pay with overtime/gross-pay-ovt-solutions.py
+++ 6. Gross pay with overtime/gross-pay-ovt-solutions.py
@@ -1,9 +1,17 @@
+# make variable input
 hours = float(input("Enter Hours: "))
 rate = float(input("Enter Rate ($): "))
 
+# make calculations to calculate gross pay by multiplying the rate by the hours worked
+# after that round the results using the built-in function round() with a exactly two digits after the decimal place.
+# but in this case we will consider the overtime using if else statement
 paid = 0
 if hours >= 40:
     paid = round(((hours-40) *1.5 )+ (rate*hours),2)
 else:
     paid = round(hours*rate,2)
+
+# Print output to the standard output device. This is using string formating.
 print(f"You must be paid the following amount: $ {paid}")
+
+# note: you can check python built-in function in https://docs.python.org/3/library/functions.html
