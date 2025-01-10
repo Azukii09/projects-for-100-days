d0c00bfb274d95825cf59fe8ffda879d040f24ff
alvillage <azuknisianta@gmail.com>
2025-01-11 05:23:33 +0700

    ## - solutions day 11 problem, make "Leap year with function" script. We will rewrite day 7 project. We change script to devine leap year or not for given number with function
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 11. Leap Year with Functions/leap-year-functions-solutions.py
+++ 11. Leap Year with Functions/leap-year-functions-solutions.py
@@ -1,6 +1,11 @@
+#Define the leap year checker function
 def is_leap_year(year):
+    # check the year inputted is leap year or not using if else statement
+    # The year must be evenly divisible by 4;
     if year % 4 == 0:
+        # If the year can also be evenly divided by 100, it is not a leap year;
         if year % 100 == 0:
+            # unless the year is also evenly divisible by 400
             if year % 400 == 0:
                 return "Leap Year."
             else:
@@ -9,7 +14,10 @@ def is_leap_year(year):
             return "Leap Year."
     else:
         return "Not Leap Year."
-
+# make variable input to input year
 year_input = int(input("Please input year: "))
-
+#Print out the result with calling the is_leap_year() function
 print(is_leap_year(year_input))
+
+
+
