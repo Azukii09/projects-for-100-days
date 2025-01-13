867db0bdc03a7539572c48e7109ba92884996a5b
alvillage <azuknisianta@gmail.com>
2025-01-14 04:23:57 +0700

    ## - solution day 14 problem, make "Maximum of three number" script for determine maximum number from three number that user inputted.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 014. Maximum of three numbers/max-three.py
+++ 014. Maximum of three numbers/max-three.py
@@ -1,3 +1,4 @@
+#define function for determine max number from 3 number
 def max_three(num1, num2, num3):
     if num1 > num2:
         if num1 > num3:
@@ -9,8 +10,10 @@ def max_three(num1, num2, num3):
     else:
         return num3
 
+#user input
 nums1 = int(input("Input numbers 1: "))
 nums2 = int(input("Input numbers 2: "))
 nums3 = int(input("Input numbers 3: "))
-
+# Print output to the standard output device.
+#with calling the function
 print(max_three(nums1, nums2, nums3))
