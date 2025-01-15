d0f8c1888b4d172bfcdd0ae96c9b4a54b8d90e69
alvillage <azuknisianta@gmail.com>
2025-01-16 00:21:26 +0700

    ## - solutions day 16 problem, make "Fizz Buzz" script for make number from 1 to 100 but every number divisible by 5 print Fizz, when divisible by 7 print Buzz, and if number divisible by both 5 and 7 print FizzBuzz.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 016. Fizz Buzz Game/fizz-buzz.py
+++ 016. Fizz Buzz Game/fizz-buzz.py
@@ -1,19 +1,26 @@
+#make loop until 100
 for i in range(1,101):
+    #check number divisible by 5, 7, and 10 for next line
     if i % 7 == 0 and i % 5 == 0 and i % 10 == 0:
         print("FizzBuzz")
         continue
+    #check number divisible by 5 and 7 for FizzBuzz
     elif i % 7 == 0 and i % 5 == 0:
         print("FizzBuzz", end=" ")
         continue
+    #check number divisible by 5 and 10 for Fizz and next line
     elif i % 5 == 0 and i % 10 == 0:
         print("Fizz")
         continue
+    #check number divisible by 5 for Fizz
     elif i % 5 == 0:
         print("Fizz", end=" ")
         continue
+    #check number divisible by 7 for Buzz
     elif i % 7 == 0:
         print("Buzz", end=" ")
         continue
+    #print number
     else:
         print(i, end=" ")
         continue
