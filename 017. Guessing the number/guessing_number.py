5ad167ced088d52b95b2eba91e654cf9a11723c8
alvillage <azuknisianta@gmail.com>
2025-01-17 04:40:40 +0700

    ## - solution day 17 problem, make "Guessing the Number" script for user to guessing the random number in 3 chance.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 017. Guessing the number/guessing_number.py
+++ 017. Guessing the number/guessing_number.py
@@ -1,18 +1,25 @@
+#import package
 import math
 import random
 
+#def function for wrong number
 def finish_wrong(number_chance):
     return f"""
 The number is {number_chance}!
     Better Luck Next time!    
     """
 
+#user input
 lower = int(input("Enter Lower bound: "))
 upper = int(input("Enter Upper bound: "))
+
+#generate random number base on user bound input
 number = random.randint(lower, upper)
+#create chance for guessing
 chance = round(math.log(upper-lower,2))
 print(f"     You've only  {chance}  chances to guess the integer!")
 
+#program logic using for loop for user guessing
 for i in range(1,chance+1):
     guess_number = int(input("Guess a number: "))
     if guess_number == number:
