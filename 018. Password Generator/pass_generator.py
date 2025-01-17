a46794948189d955a9776d5e6701f8be1963abfb
alvillage <azuknisianta@gmail.com>
2025-01-18 04:29:04 +0700

    ## - solution day 18 problem, make "Password Generator" script for make strong password base on user input.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 018. Password Generator/pass_generator.py
+++ 018. Password Generator/pass_generator.py
@@ -1,25 +1,30 @@
+#import package
 from random import choice, shuffle
-
+#set up character variable
 letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 nums = "1234567890"
 symbols = "-+=!@#$%^&*"
 
+#function for random choice character base on user input
 def for_loop_password(loops_numb, chose_letters):
     return [choice(chose_letters) for _ in range(loops_numb)]
 
-
+#function for shuffling chosen character
 def shuffle_password(p_list):
     shuffle(p_list)
     return "".join(p_list)
 
+#input password criteria
 password=[]
 p_letter = int(input("How many letters do you want in your password? "))
 p_numbs=int(input("How many numbers do you want in your password? "))
 p_symbols=int(input("How many symbols do you want in your password? "))
 
+#generate password base on user criteria
 password.extend(for_loop_password(p_letter, letters))
 password.extend(for_loop_password(p_numbs, nums))
 password.extend(for_loop_password(p_symbols, symbols))
 
+#print final output from password that shuffled
 print(shuffle_password(password))
 
