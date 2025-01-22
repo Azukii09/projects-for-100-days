6faec3306dc52ccebb2de5a0046fd3dc584d2825
alvillage <azuknisianta@gmail.com>
2025-01-23 05:41:02 +0700

    ## - solution day 23 problem, make "Cryptography - Decryption" script for decrypt password using Caesar Cipher strategy.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 023. Cryptography with Python - Caesar Cipher - Decryption/cryptography_project.py
+++ 023. Cryptography with Python - Caesar Cipher - Decryption/cryptography_project.py
@@ -1,7 +1,10 @@
+#import package
 from decryption_cryptography import  decryption_function
 
+#run program
 print("Decryption!")
 text_d = input("Input Text: ")
 shifter_d = int(input("Input Shift number: "))
 
+# call function
 print(decryption_function(text_d, shifter_d))
