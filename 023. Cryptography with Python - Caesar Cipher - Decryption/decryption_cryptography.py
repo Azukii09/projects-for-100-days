fa436e26cd295267d68a39ebd20263627ef82e05
alvillage <azuknisianta@gmail.com>
2025-01-23 05:40:20 +0700

    ## - solution day 23 problem, make "Cryptography - Decryption" script for decrypt password using Caesar Cipher strategy.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 023. Cryptography with Python - Caesar Cipher - Decryption/decryption_cryptography.py
+++ 023. Cryptography with Python - Caesar Cipher - Decryption/decryption_cryptography.py
@@ -1,14 +1,20 @@
+#define decrypt function
 def decryption_function(text, key):
+    #define character
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    #define var for output encryption
     new_text = list(text.upper())
     a = list()
+    #make shifter and index for char
     sifter = key % 26
     index_char = list()
+    #logic for get char input index in alphabet var
     for char in new_text:
         if char in alphabet:
             index_char.append(alphabet.index(char))
         else:
             index_char.append(char)
+    #logic for shifting char base on sifter index
     for i in index_char:
         if isinstance(i, int) and i-sifter < 0:
             a.append(alphabet[(26-(sifter-i))])
@@ -16,4 +22,5 @@ def decryption_function(text, key):
             a.append(alphabet[i-sifter])
         else:
             a.append(i)
+    #return encrypted password
     return "".join(a)
