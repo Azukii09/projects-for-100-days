38a82fd3f24666f8bfde26d18f0681d4dba7cb60
alvillage <azuknisianta@gmail.com>
2025-01-24 05:14:46 +0700

    ## - day 24 problem, make "Hangman" script for apply all knowledge we know.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 024. Hangman/hangman_main.py
+++ 024. Hangman/hangman_main.py
@@ -1,19 +1,27 @@
+#set up initial game
 from secret_word import choosing_word
 from logo_hangman import logo, hangman_stages
 missing_word = choosing_word()
 guesses = list()
+
+#setup blanked list for guessing the word
 for letter in missing_word:
     guesses.append("_")
 live = 6
 print(logo)
+
+#game logic
 while True:
     print(hangman_stages[live])
+    #checking for live
     if live>0:
         print(missing_word)
         print(" ".join(guesses))
         print("You have {} guesses remaining.".format(live))
         user_guess = input("Guess a letter: ").upper()
         print("\033[2J")
+
+        #guessing logic
         if user_guess not in guesses:
             for index, letter in enumerate(missing_word):
                 if user_guess == letter:
--- 024. Hangman/secret_word.py
+++ 024. Hangman/secret_word.py
@@ -1,6 +1,7 @@
 import random
 import word_list
 
+#chosing random word from list of word
 def choosing_word():
     word = random.choice(word_list.word_list)
     return list(word)
