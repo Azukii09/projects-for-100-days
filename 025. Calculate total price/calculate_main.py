cc5df622e864f1e87edd6fb7f183689fb6d63493
alvillage <azuknisianta@gmail.com>
2025-01-25 05:48:51 +0700

    ## - day 25 problem, make "Price Calculator" script for count the total price that user must be pay.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- 025. Calculate total price/calculate_main.py
+++ 025. Calculate total price/calculate_main.py
@@ -1,11 +1,4 @@
-# computer - price: 500, quantity : 10
-# monitor - price: 200, quantity : 8
-# keyboard - price: 500, quantity : 5
-# mouse - price: 10, quantity : 0
-# hdmi cable - price: 20, quantity : 7
-# dvd drive - price: 50, quantity : 5
-from itertools import count
-
+#define nested dictionary for item data
 item = {
     "1":["computer",{ "price": 500, "quantity" : 10}],
     "2":["monitor",{"price": 200, "quantity": 8}],
@@ -14,12 +7,23 @@ item = {
     "5":["hdmi cable",{ "price": 20, "quantity" : 7}],
     "6":["dvd drive",{ "price": 50, "quantity" : 5}],
 }
+#define temp var for count total price
 count_total = 0
-for index, chose_item in enumerate(item.values()):
-    print(f"{index+1}. {chose_item[0]}")
-print("0. Finish")
+
+#program logic
 while True:
+    #print out the item list
+    for index, chose_item in enumerate(item.values()):
+        #check quantity
+        if chose_item[1]["quantity"] > 0:
+            print(f"{index+1}. {chose_item[0]}")
+        else:
+            print(f"{index+1}. {chose_item[0]} (out of stock)")
+    print("0. Finish")
+
+    #user input
     user_choice = int(input("Enter your choice: "))
+    #logic for user current item choice
     if user_choice == 0:
         print(f"Total price: {count_total}")
         break
