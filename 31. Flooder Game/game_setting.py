9161bfd187a221db137bb08e09ba8c94fef17c71
alvillage <azuknisianta@gmail.com>
2025-01-31 04:42:58 +0700

    ## - solution day 31 problem, make "Flooder Game" script for simulating of flooder game.
    Signed-off-by: alvillage <azuknisianta@gmail.com>

---
+++ 31. Flooder Game/game_setting.py
@@ -0,0 +1,20 @@
+tile_types = (0,1,2,3,4,5)
+colors_map = {
+    0: "\033[;;41m  ",
+    1: "\033[;;42m  ",
+    2: "\033[;;44m  ",
+    3: "\033[;;103m  ",
+    4:"\033[;;46m  ",
+    5: "\033[;;45m  "
+}
+no_color="\033[;;m"
+board_width = 20
+board_height = 12
+moves_per_game = 20
+left_right = chr(9472)*2
+up_down = chr(9474)
+down_right = chr(9484)
+down_left = chr(9488)
+up_right = chr(9492)
+up_left = chr(9496)
+block = chr(9608)
